977c6dad42260d02539ce4b60bb6d6cf9ca04198
wojciech_dymek <wojciechdymek94@gmail.com>
2017-03-25 15:11:14 +0100

    added gitignore

--- lab02/preprocessing_service.rb
+++ lab02/preprocessing_service.rb
@@ -8,8 +8,8 @@ def process_line(line)
    result = result.gsub(/[:]/i, ' ')
    
    #remove non alphanumeric symbols
-   #result = result.gsub(/[^0-9a-z ]/i, '')
-   result = result.gsub(/[^a-z ]/i, '')
+   result = result.gsub(/[^0-9a-z ]/i, '')
+   3result = result.gsub(/[^a-z ]/i, '')
    
    return result
  end
